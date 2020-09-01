6e458f9366ee866155e972afa7f951f663453f18
Haz-git <zhouharry1@gmail.com>
2020-09-01 08:34:42 -0700

    Created Pre-made Axios file with API Key

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyDsORy3S7h4_W_ZaygrisCWSPMV7Nvodd0';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+});
